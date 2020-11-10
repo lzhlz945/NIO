ba3fd5dbcdd1352d38f6a1f4a0b53458b6c53577
Administrator <1746678613@qq.com>
2020-11-10 09:34:39 +0800

    bran1 --IO与NIO的区别

---
+++ src/main/java/com/zhang/ioAndNio/Deffer.java
@@ -0,0 +1,25 @@
+package com.zhang.ioAndNio;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.ioAndNio
+ * @date:2020/11/10
+ */
+public class Deffer {
+    /**
+     * IO与NIO的区别
+     * IO:是面向流的；是单向的，输入、输出流
+     *    是阻塞式IO，没有选择器
+     * NIO：面向通道，缓冲区（铁路类似），数据传输利用缓冲区（火车类似）；是双向的
+     *      非阻塞IO,有选择器。
+     */
+ @Test
+ public void test01(){
+
+ }
+
+
+}
